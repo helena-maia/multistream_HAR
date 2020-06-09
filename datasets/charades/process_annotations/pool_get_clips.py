79648de45511416857262ff537ba1733ae42d238
Helena <helena-maia>
2020-06-09 10:47:19 -0300

    pool get_clips

--- datasets/charades/process_annotations/pool_get_clips.py
+++ datasets/charades/process_annotations/pool_get_clips.py
@@ -3,6 +3,41 @@ import os
 import numpy as np
 import shutil
 
+def copy_video(info):
+    video_id, class_id, frame_start, frame_end, segm_ind = info[:5]
+    src_img_fmt, src_flow_fmt, dst_dir, dst_img_fmt, dst_flow_fmt = info[5:]
+    frame_start, frame_end = int(frame_start) + 1, int(frame_end) + 1
+    dst_dir = dst_dir_fmt%(class_id, video_id, class_id, segm_ind)
+
+    if not os.path.isdir(dst_dir):
+        print("Creating video directory: ", dst_dir)
+        os.makedirs(dst_dir)
+
+    count = 1
+
+    for i in range(frame_start, frame_end + 1):
+        src_img = src_img_fmt%(video_id, video_id, j)
+        src_flow_x = src_flow_fmt%(video_id, video_id, j, "x")
+        src_flow_y = src_flow_fmt%(video_id, video_id, j, "y")
+        if not (os.path.isfile(src_img) and os.path.isfile(src_flow_x) and os.path.isfile(src_flow_y)):
+            continue
+
+        dst_img = os.path.join(dst_dir, dst_img_fmt%count)
+        dst_flow_x = os.path.join(dst_dir, dst_flow_fmt%("x", count))
+        dst_flow_y = os.path.join(dst_dir, dst_flow_fmt%("y", count))
+
+        shutil.copy(src_img, dst_img)
+        shutil.copy(src_flow_x, dst_flow_x)
+        shutil.copy(src_flow_y, dst_flow_y)
+
+        #print(src_img, dst_img)
+        #print(src_flow_x, dst_flow_x)
+        #print(src_flow_y, dst_flow_y)
+
+        count += 1
+
+
+
 def crop(list_path, src_path, dst_path):
     final_list = []
 
