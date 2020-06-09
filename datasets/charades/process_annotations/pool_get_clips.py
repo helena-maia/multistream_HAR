7ad50ed3a35e832f77cca6b4c81e4cc8685b4088
helena-maia <helena.almaia@gmail.com>
2020-06-09 11:58:28 -0300

    update pool_get_clips

--- datasets/charades/process_annotations/pool_get_clips.py
+++ datasets/charades/process_annotations/pool_get_clips.py
@@ -1,13 +1,17 @@
+from multiprocessing import Pool
 import csv
 import os
 import numpy as np
 import shutil
 
+src_img_fmt, src_flow_fmt, dst_dir, dst_img_fmt, dst_flow_fmt = "", "", "", "", ""
+
 def copy_video(info):
-    video_id, class_id, frame_start, frame_end, segm_ind = info[:5]
-    src_img_fmt, src_flow_fmt, dst_dir, dst_img_fmt, dst_flow_fmt = info[5:]
+    video_id, class_id, frame_start, frame_end, segm_ind = info
     frame_start, frame_end = int(frame_start) + 1, int(frame_end) + 1
     dst_dir = dst_dir_fmt%(class_id, video_id, class_id, segm_ind)
+
+    print(dst_dir)
 
     if not os.path.isdir(dst_dir):
         print("Creating video directory: ", dst_dir)
@@ -19,6 +23,7 @@ def copy_video(info):
         src_img = src_img_fmt%(video_id, video_id, j)
         src_flow_x = src_flow_fmt%(video_id, video_id, j, "x")
         src_flow_y = src_flow_fmt%(video_id, video_id, j, "y")
+
         if not (os.path.isfile(src_img) and os.path.isfile(src_flow_x) and os.path.isfile(src_flow_y)):
             continue
 
@@ -30,16 +35,15 @@ def copy_video(info):
         shutil.copy(src_flow_x, dst_flow_x)
         shutil.copy(src_flow_y, dst_flow_y)
 
-        #print(src_img, dst_img)
-        #print(src_flow_x, dst_flow_x)
-        #print(src_flow_y, dst_flow_y)
-
         count += 1
 
+    #relative_path = "%s/%s_%s_%s"%(class_id, video_id, class_id, segm_ind)
+    #label = int(class_id[1:])
+    #return [relative_path, count, label]
 
 
-def crop(list_path, src_path, dst_path):
-    final_list = []
+def crop(list_path, src_path, dst_path, num_worker):
+    global src_img_fmt, src_flow_fmt, dst_dir, dst_img_fmt, dst_flow_fmt
 
     with open(list_path, "r") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=' ')
@@ -51,42 +55,8 @@ def crop(list_path, src_path, dst_path):
         dst_img_fmt = "img_%05d.jpg"
         dst_flow_fmt = "flow_%s_%05d.jpg"
 
-        for i, line in enumerate(csv_reader):
-            video_id, class_id, frame_start, frame_end, segm_ind = line
-            frame_start, frame_end = int(frame_start) + 1, int(frame_end) + 1
-            dst_dir = dst_dir_fmt%(class_id, video_id, class_id, segm_ind)
-
-            if not os.path.isdir(dst_dir):
-                print("Creating video directory: ", dst_dir)
-                os.makedirs(dst_dir)
-
-            count = 1
-            for j in range(frame_start, frame_end+1):
-                src_img = src_img_fmt%(video_id, video_id, j)
-                src_flow_x = src_flow_fmt%(video_id, video_id, j, "x")
-                src_flow_y = src_flow_fmt%(video_id, video_id, j, "y")
-                if not (os.path.isfile(src_img) and os.path.isfile(src_flow_x) and os.path.isfile(src_flow_y)):
-                    continue
-
-                dst_img = os.path.join(dst_dir, dst_img_fmt%count)
-                dst_flow_x = os.path.join(dst_dir, dst_flow_fmt%("x", count))
-                dst_flow_y = os.path.join(dst_dir, dst_flow_fmt%("y", count))
-
-                shutil.copy(src_img, dst_img)
-                shutil.copy(src_flow_x, dst_flow_x)
-                shutil.copy(src_flow_y, dst_flow_y)
-
-                #print(src_img, dst_img)
-                #print(src_flow_x, dst_flow_x)
-                #print(src_flow_y, dst_flow_y)
-
-                count += 1
-
-            relative_path = "%s/%s_%s_%s"%(class_id, video_id, class_id, segm_ind)
-            label = int(class_id[1:])
-            final_list.append([relative_path, count, label])
-
-    return final_list
+        pool = Pool(num_worker)
+        pool.map(copy_video, csv_reader)
 
 
 src_path = ["/home/helena.maia/Documentos/Datasets/Charades_v1_rgb/",
@@ -94,14 +64,11 @@ src_path = ["/home/helena.maia/Documentos/Datasets/Charades_v1_rgb/",
 train_subclips = "Charades_v1_train_clips.csv"
 test_subclips = "Charades_v1_test_clips.csv"
 dst_path = "/home/helena.maia/Documentos/Datasets/Charades_v1_subclips/"
+num_worker = 8
 
 if not os.path.isdir(dst_path):
     print("Creating output directory: ", dst_path)
     os.makedirs(dst_path)
 
-train_list = sorted(crop(train_subclips, src_path, dst_path))
-test_list = sorted(crop(test_subclips, src_path, dst_path))
-
-np.savetxt("train.txt", train_list, fmt="%s")
-np.savetxt("test.txt", test_list, fmt="%s")
-
+crop(train_subclips, src_path, dst_path, num_worker)
+crop(test_subclips, src_path, dst_path, num_worker)
