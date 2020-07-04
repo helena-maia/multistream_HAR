db36047232311f88009a7e83376031f149ee79a3
Helena <helena.almaia@gmail.com>
2020-07-04 02:48:39 -0300

    update

--- AVR_earlystop/experiment_summary.py
+++ AVR_earlystop/experiment_summary.py
@@ -1,6 +1,24 @@
 import glob
 import json
+import os
 
-log_list = glob.glob("log/*/args.json")
-print(log_list)
+log_list = glob.glob("log/*")
+
+for ll in log_list:
+	args = os.path.join(ll, "args.json")
+	early_stop = os.path.join(ll, "early_stopping.json")
+
+	args_dict, early_dict = {}, {}
+
+
+	with open(args) as f:
+		args_dict = json.load(f)
+
+	with open(early_stop) as f:
+		early_dict = json.load(f)
+
+    print(args_dict.keys())
+    print(early_dict.keys())
+
+
 
