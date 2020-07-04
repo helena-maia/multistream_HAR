4e228df3d7ded38e18cb4b626257b0ef2b37d705
Helena <helena.almaia@gmail.com>
2020-07-04 03:43:11 -0300

    update

--- AVR_earlystop/experiment_summary.py
+++ AVR_earlystop/experiment_summary.py
@@ -28,10 +28,13 @@ for ll in log_list:
 
     timestamp = ll.split("/")[1]
     data = [args_dict[k] for k in header[1:-2]]
-    best_epoch = sorted([k for k in early_dict.keys() if k != "config"])[-8]
-    best_data = early_dict[best_epoch]
-    data = [timestamp] + data + [best_epoch, best_data["val_loss"]]
-    summary += data
+
+    try:
+        best_epoch = sorted([k for k in early_dict.keys() if k != "config"])[-8]
+    else:
+        best_data = early_dict[best_epoch]
+        data = [timestamp] + data + [best_epoch, best_data["val_loss"]]
+        summary += data
 
 summary = np.savetxt("summary.txt", fmt="%s")
 
