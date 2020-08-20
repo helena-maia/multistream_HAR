9aac1e612a7ac07b9d45611fb5531e8292d39824
helena-maia <helena.almaia@gmail.com>
2020-08-20 14:00:22 -0300

    update

--- AVR_earlystop/fusion/every_combination2.py
+++ AVR_earlystop/fusion/every_combination2.py
@@ -19,14 +19,13 @@ for r in range(2, n_modalities+1): # combinations of 2 or more modalities
     c = itertools.combinations(u_modalities, r=r)
     comb = itertools.chain(comb, c) if comb else c
 
+output = []
 for c in comb:		
 	for d in u_datasets:
-		print(d)
 		npys2 = npys1[datasets == d]
 		splits = npys2[:, 3]
 
 		for s in u_splits:
-			print(s)
 			npys3 = npys2[splits == str(s)]
 			modalities = npys3[:, 4]
 
@@ -37,5 +36,6 @@ for c in comb:
 
 			args = argparse.Namespace(d=d, m=method, npy_paths=npy_paths, s=s, settings=settings)
 
-			_, _, prec = fusion(args)
-			print(prec)
+			best_weight, _, prec = fusion(args)
+			output.append("{}\t{}\t{}\t{}\t{}\t{}".format(method, d, s, c.join(" + "), best_weight, prec))
+			print(output[-1])
