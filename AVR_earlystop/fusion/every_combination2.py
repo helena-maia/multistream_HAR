cf897d1adff302dcc050987d1754813e31e55534
Helena <helena-maia>
2020-08-21 05:08:33 -0300

    update

--- AVR_earlystop/fusion/every_combination2.py
+++ AVR_earlystop/fusion/every_combination2.py
@@ -9,17 +9,16 @@ datasets = npys1[:, 5]
 
 u_modalities = ["RGB", "FLOW", "AVR", "LVR0", "LVR1", "LVR2"]
 u_datasets = ["ucf101", "hmdb51"]
-u_splits = [1, 2, 3]
+u_splits = [1]
 settings = "../../AVR_earlystop/datasets/settings_earlystop/"
-method = "weighted_avg"
+method = "choquet_fuzzy"
 
 n_modalities = len(u_modalities)
 comb = None
-for r in range(6, n_modalities+1): # combinations of 2 or more modalities
+for r in range(2, n_modalities+1): # combinations of 2 or more modalities
     c = itertools.combinations(u_modalities, r=r)
     comb = itertools.chain(comb, c) if comb else c
 
-output = []
 for c in comb:
 	for d in u_datasets:
 		npys2 = npys1[datasets == d]
@@ -37,6 +36,7 @@ for c in comb:
 			args = argparse.Namespace(d=d, m=method, npy_paths=npy_paths, s=s, settings=settings)
 
 			best_weight, _, prec = fusion(args)
-			output.append("{}\t{}\t{}\t{}\t{}\t{}".format(method, d, s, " + ".join(list(c)) , best_weight, prec))
-			print(output[-1])
+			print(d, s, " + ".join(list(c)))
+			with open("fusion_choquet.out", 'a') as f:
+				f.write("{}\t{}\t{}\t{}\t{}\t{}".format(method, d, s, " + ".join(list(c)) , best_weight, prec))
 
