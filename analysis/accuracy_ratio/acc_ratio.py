132ded4bc667c305c6dec4078f89647cfc91ba62
Helena <helena-maia>
2020-09-03 03:42:24 -0300

    update

--- analysis/accuracy_ratio/acc_ratio.py
+++ analysis/accuracy_ratio/acc_ratio.py
@@ -9,9 +9,8 @@ datasets = npy_list[:, 5]
 u_modalities = ["RGB", "FLOW", "AVR", "LVR0", "LVR1", "LVR2"]
 u_datasets = ["ucf101", "hmdb51"]
 settings = "../../AVR_earlystop/datasets/settings_earlystop/"
-colors = plt.get_cmap('Set1').colors
-markers = ['o','x','d','+','v','s']
-
+colors = plt.get_cmap('Dark2').colors
+markers = ['s','o','d','v','x','+'] 
 
 for i, d in enumerate(u_datasets):
     class_path = os.path.join(settings, "%s/class_ind.txt" % (d))
@@ -31,9 +30,14 @@ for i, d in enumerate(u_datasets):
 
     class_acc_list = np.array(class_acc_list)
 
-    class_acc_list = class_acc_list[:,51:]
-    class_ind = class_ind[51:,1]
-    print(class_ind.shape)
+    max = np.max(class_acc_list, axis=0)
+    asort = (np.argsort(max)[::-1])[:51]
+    class_acc_list = np.array([class_acc_list[:,ind] for ind in asort]).transpose()
+    class_ind = np.array([class_ind[ind] for ind in asort])[:,1]
+
+    #class_acc_list = class_acc_list[:,51:]
+    #class_ind = class_ind[51:,1]
+    #print(class_ind.shape)
 
     plt.ylim(bottom=0., top=101.)
     plt.xlim(left=-1, right=class_acc_list.shape[1])
@@ -41,7 +45,7 @@ for i, d in enumerate(u_datasets):
     plt.ylabel('Recall', fontsize=12)
     #plt.xlabel('Class', fontsize=12)
     plt.xticks(np.arange(0,class_acc_list.shape[1]), class_ind, rotation='vertical')
-    plt.yticks(np.arange(0,100,20))
+    plt.yticks(np.arange(0,101,20))
 
     p = []
     for k in range(6):
