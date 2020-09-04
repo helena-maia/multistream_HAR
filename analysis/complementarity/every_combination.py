e05bbbe562059221ed76a4f7635f55bc191bfc9d
helena-maia <helena.almaia@gmail.com>
2020-09-04 18:52:33 -0300

    update

--- analysis/complementarity/every_combination.py
+++ analysis/complementarity/every_combination.py
@@ -17,27 +17,26 @@ for r in range(2, n_modalities+1): # combinations of 2 or more modalities
     c = itertools.combinations(u_modalities, r=r)
     comb = itertools.chain(comb, c) if comb else c
 
-for d in u_datasets:
-    npys2 = npys1[u_datasets == d]
-    splits = npys2[:, 3]
+for c in comb:
+    print(c)
+    for d in u_datasets:
+        npys2 = npys1[u_datasets == d]
+        splits = npys2[:, 3]
 
-    for c in comb:
-            print(c, end=" ")
-            accum_it = 0.
+        accum_it = 0.
+        for s in u_splits:
+            npys3 = npys2[splits == str(s)]
+            modalities = npys3[:, 4]
 
-            for s in u_splits:
-                npys3 = npys2[splits == str(s)]
-                modalities = npys3[:, 4]
+            test_path = os.path.join(settings, "%s/test_split%s.txt" % (d, s))
 
-                test_path = os.path.join(settings, "%s/test_split%s.txt" % (d, s))
+            npy_paths = []
+            for m in c:
+                n = npys3[modalities == m][0]
+                npy_paths.append(os.path.join(n[0], n[1], n[2]))
+        
+            it = interrater(npy_paths, test_path)
+            accum_it += it
 
-                npy_paths = []
-                for m in c:
-                    n = npys3[modalities == m][0]
-                    npy_paths.append(os.path.join(n[0], n[1], n[2]))
-            
-                it = interrater(npy_paths, test_path)
-                accum_it += it
-
-            accum_it /= 3.
-            print(accum_it)
+        accum_it /= 3.
+        print(accum_it)
