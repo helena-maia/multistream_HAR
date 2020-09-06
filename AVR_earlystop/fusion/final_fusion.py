8bcdbc91d4d939d5f48a98f87d0635dd1fe41b19
helena-maia <helena.almaia@gmail.com>
2020-09-06 03:29:27 -0300

    update

--- AVR_earlystop/fusion/final_fusion.py
+++ AVR_earlystop/fusion/final_fusion.py
@@ -24,46 +24,46 @@ parameters['hmdb51'] = [{'C':10., 'kernel': 'rbf', 'gamma': 10**(-3), 'ms': 'ovr
                         {'C':10.**(-2), 'kernel': 'linear', 'gamma': 10**(-4), 'ms': 'ovr'}]
 
 for d in u_datasets:
-	npys2 = npys1[datasets == d]
-	splits = npys2[:, 3]
+    npys2 = npys1[datasets == d]
+    splits = npys2[:, 3]
 
-	for s in u_splits:
-		npys3 = npys2[splits == str(s)]
-		modalities = npys3[:, 4]
+    for s in u_splits:
+        npys3 = npys2[splits == str(s)]
+        modalities = npys3[:, 4]
 
-		npy_data = []
-		for m in combination[d]:
-			n = npys3[modalities == m][0]
-			npy_path = os.path.join(n[0], n[1], n[2])
-			npy_data.append(np.load(npy_path))
+        npy_data = []
+        for m in combination[d]:
+            n = npys3[modalities == m][0]
+            npy_path = os.path.join(n[0], n[1], n[2])
+            npy_data.append(np.load(npy_path))
 
         split_path = os.path.join(settings, "%s/%s_split%s.txt" % (d, "%s", s))
-		train_path = split_path%("train")
-		val_path = split_path%("val")
-		test_path = split_path%("test")
-		dataset_path = os.path.join(args.settings, "%s/dataset_list.txt" % (args.d))
+        train_path = split_path%("train")
+        val_path = split_path%("val")
+        test_path = split_path%("test")
+        dataset_path = os.path.join(args.settings, "%s/dataset_list.txt" % (args.d))
 
         ds_keys, ds_labels = get_labels(dataset_path)
-		tr_keys, tr_labels = get_labels(train_path)
-    	vl_keys, vl_labels = get_labels(val_path)
-    	ts_keys, ts_labels = get_labels(test_path)
+        tr_keys, tr_labels = get_labels(train_path)
+        vl_keys, vl_labels = get_labels(val_path)
+        ts_keys, ts_labels = get_labels(test_path)
 
-		tr_ind = [ds_keys.index(key) for ind, key in enumerate(tr_keys)]
-	    vl_ind = [ds_keys.index(key) for ind, key in enumerate(vl_keys)]
-	    ts_ind = [ds_keys.index(key) for ind, key in enumerate(ts_keys)]
+        tr_ind = [ds_keys.index(key) for ind, key in enumerate(tr_keys)]
+        vl_ind = [ds_keys.index(key) for ind, key in enumerate(vl_keys)]
+        ts_ind = [ds_keys.index(key) for ind, key in enumerate(ts_keys)]
 
-	    X_tr, X_vl, X_ts = [], [], []
-	    for npy in npy_data:
-	        tr, vl, ts = npy[tr_ind], npy[vl_ind], npy[ts_ind]
-	        X_tr.append(tr)
-	        X_vl.append(vl)
-	        X_ts.append(ts)
+        X_tr, X_vl, X_ts = [], [], []
+        for npy in npy_data:
+            tr, vl, ts = npy[tr_ind], npy[vl_ind], npy[ts_ind]
+            X_tr.append(tr)
+            X_vl.append(vl)
+            X_ts.append(ts)
 
-	    X_tr, X_vl, X_ts = np.array(X_tr), np.array(X_vl), np.array(X_ts)
-	    y_tr, y_vl, y_ts = np.array(tr_labels), np.array(vl_labels), np.array(ts_labels)
+        X_tr, X_vl, X_ts = np.array(X_tr), np.array(X_vl), np.array(X_ts)
+        y_tr, y_vl, y_ts = np.array(tr_labels), np.array(vl_labels), np.array(ts_labels)
 
-	    y_pred, prec = fixed_SVM(X_tr, X_vl, X_ts, y_tr, y_vl, y_ts)
+        y_pred, prec = fixed_SVM(X_tr, X_vl, X_ts, y_tr, y_vl, y_ts)
 
-	    np.save("svm_"+d+"_"+str(s)+".npy", y_pred)
+        np.save("svm_"+d+"_"+str(s)+".npy", y_pred)
 
 
